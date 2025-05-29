6e6b9e13e0a289f822d04f8fb18ecf4075208d24
John <j.ooi@student.fontys.nl>
2025-05-29 16:47:44 +0200

    dependencies added

--- src/main/java/com/tokentrackr/crypto_integration_service/model/CryptoData.java
+++ src/main/java/com/tokentrackr/crypto_integration_service/model/CryptoData.java
@@ -1,5 +1,6 @@
 package com.tokentrackr.crypto_integration_service.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CryptoData {
     private String id;
     private String symbol;
